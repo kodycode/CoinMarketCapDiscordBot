ad6a2cf220bb3507534ad8308ef0ceb5e5c9bb5f
kodycode <kodythach@gmail.com>
2018-02-10 00:09:00 -0500

    Finished the rest of implementation

--- cogs/alert_cmd_handler.py
+++ cogs/alert_cmd_handler.py
@@ -10,7 +10,7 @@ class AlertCommands:
     @commands.command(name='adda', pass_context=True)
     async def adda(self, ctx, currency: str, operator: str, price: float, fiat='USD'):
         """
-        Adds an alert notification for the user when a price meets the condition specified
+        Adds alert for when the price of crypto meets the condition given
         An example for this command would be:
         "$adda bitcoin <= 15000"
 
@@ -25,12 +25,12 @@ class AlertCommands:
         """
         await self.cmd_function.alert.add_alert(ctx, currency, operator, price, fiat)
 
-    @commands.command(name='adda1h', pass_context=True)
-    async def addahour(self, ctx, currency: str, operator: str, percent: str, fiat='USD'):
+    @commands.command(name='addah', pass_context=True)
+    async def addahour(self, ctx, currency: str, operator: str, percent: float, fiat='USD'):
         """
-        Adds an alert notification for the user when a price meets the condition specified
+        Adds alert for when percent change in 1h of crypto meets the condition given
         An example for this command would be:
-        "$adda1h bitcoin 2.5%"
+        "$addah bitcoin >= 2.5"
 
         @param currency - cryptocurrency to set an alert of
         @param operator - operator for the given choices
@@ -41,14 +41,14 @@ class AlertCommands:
         @param price - percent of 1h change to compare
         @param fiat - desired fiat currency (i.e. 'EUR', 'USD')
         """
-        await self.cmd_function.alert.add_alert(ctx, currency, operator, percent, fiat)
+        await self.cmd_function.alert.add_alert(ctx, currency, operator, percent, fiat, hour=True)
 
-    @commands.command(name='adda24h', pass_context=True)
-    async def addaday(self, ctx, currency: str, operator: str, percent: str, fiat='USD'):
+    @commands.command(name='addad', pass_context=True)
+    async def addaday(self, ctx, currency: str, operator: str, percent: float, fiat='USD'):
         """
-        Adds an alert notification for the user when a price meets the condition specified
+        Adds alert for when percent change in 24h of crypto meets the condition given
         An example for this command would be:
-        "$adda bitcoin <= 15000"
+        "$addad bitcoin >= 2.5"
 
         @param currency - cryptocurrency to set an alert of
         @param operator - operator for the given choices
@@ -56,17 +56,17 @@ class AlertCommands:
                           <= - less than or equal to
                           >  - greater than
                           >= - greater than or equal to
-        @param price - percent of 1h change to compare
+        @param price - percent of a day's change to compare
         @param fiat - desired fiat currency (i.e. 'EUR', 'USD')
         """
-        await self.cmd_function.alert.add_alert(ctx, currency, operator, percent, fiat)
+        await self.cmd_function.alert.add_alert(ctx, currency, operator, percent, fiat, day=True)
 
     @commands.command(name='addaw', pass_context=True)
-    async def addaweek(self, ctx, currency: str, operator: str, percent: str, fiat='USD'):
+    async def addaweek(self, ctx, currency: str, operator: str, percent: float, fiat='USD'):
         """
-        Adds an alert notification for the user when a price meets the condition specified
+        Adds alert for when percent change in 7d of crypto meets the condition given
         An example for this command would be:
-        "$adda bitcoin <= 15000"
+        "$addaw bitcoin >= 2.5"
 
         @param currency - cryptocurrency to set an alert of
         @param operator - operator for the given choices
@@ -74,10 +74,10 @@ class AlertCommands:
                           <= - less than or equal to
                           >  - greater than
                           >= - greater than or equal to
-        @param price - percent of 1h change to compare
+        @param price - percent of a week's change to compare
         @param fiat - desired fiat currency (i.e. 'EUR', 'USD')
         """
-        await self.cmd_function.alert.add_alert(ctx, currency, operator, percent, fiat)
+        await self.cmd_function.alert.add_alert(ctx, currency, operator, percent, fiat, week=True)
 
     @commands.command(name='rema', pass_context=True)
     async def rema(self, ctx, alert_num: str):
